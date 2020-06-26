63b9a5ba0d9d883138572d6df898421a3ed9c857
Janne Savolainen <janne.savolainen@houston-inc.com>
2020-06-26 16:19:09 +0300

    Build packages to work with require and imports

--- packages/core/configs/sharedWebpackConfig.js
+++ packages/core/configs/sharedWebpackConfig.js
@@ -7,6 +7,8 @@ module.exports = {
   output: {
     path: path.resolve(process.cwd(), 'build'),
     filename: 'index.js',
+    libraryTarget: 'commonjs2',
+    libraryExport: 'default',
   },
   node: {
     __dirname: true,
