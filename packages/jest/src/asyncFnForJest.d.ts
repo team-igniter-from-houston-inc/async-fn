f328c1e57d0de135cd43c228630fb4de735e44da
Janne Savolainen <janne.savolainen@live.fi>
2021-12-27 13:56:17 +0200

    Make resolved value mandatory in typing

--- packages/jest/src/asyncFnForJest.d.ts
+++ packages/jest/src/asyncFnForJest.d.ts
@@ -1,12 +1,16 @@
 declare module '@async-fn/jest' {
   import type { MockInstance } from 'jest-mock';
 
+  type TentativeTuple<T> = T extends undefined ? [undefined?] : [T];
+
   type AsyncFnMock<
     TToBeMocked extends (...args: unknown[]) => unknown,
     TArguments extends Parameters<TToBeMocked> = Parameters<TToBeMocked>,
-    TResolve extends Awaited<ReturnType<TToBeMocked>> = Awaited<ReturnType<TToBeMocked>>
+    TResolve extends Awaited<ReturnType<TToBeMocked>> = Awaited<
+      ReturnType<TToBeMocked>
+    >
   > = MockInstance<(TArguments) => Promise<TResolve>, TArguments> & {
-    resolve: (resolvedValue: TResolve) => Promise<void>;
+    resolve: (...resolvedValue: TentativeTuple<TResolve>) => Promise<void>;
     reject: (rejectValue?: any) => Promise<void>;
   } & ((...args: TArguments) => Promise<TResolve>);
 
