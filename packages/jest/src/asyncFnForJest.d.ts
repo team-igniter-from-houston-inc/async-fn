2d5e1c161de699f1d7912bd6a81ee158ea33b5cd
Sebastian Malton <sebastian@malton.name>
2022-06-22 15:22:16 -0400

    Fix support for non-promises

--- packages/jest/src/asyncFnForJest.d.ts
+++ packages/jest/src/asyncFnForJest.d.ts
@@ -4,13 +4,13 @@ declare module '@async-fn/jest' {
   type TentativeTuple<T> = T extends undefined ? [undefined?] : [T];
 
   type AsyncFnMock<TToBeMocked extends (...args: any[]) => any> = jest.MockedFunction<TToBeMocked> & {
-    resolve: (...resolvedValue: TentativeTuple<ReturnType<TToBeMocked>>) => Promise<void>;
+    resolve: (...resolvedValue: TentativeTuple<ReturnType<TToBeMocked> | Awaited<ReturnType<TToBeMocked>>>) => Promise<void>;
 
     resolveSpecific: (
       callFinder:
         | PartialDeep<Parameters<TToBeMocked>>
         | ((parameters: Parameters<TToBeMocked>) => boolean),
-      ...resolvedValue: TentativeTuple<ReturnType<TToBeMocked>>
+      ...resolvedValue: TentativeTuple<ReturnType<TToBeMocked> | Awaited<ReturnType<TToBeMocked>>>
     ) => Promise<void>;
 
     reject: (rejectValue?: any) => Promise<void>;
